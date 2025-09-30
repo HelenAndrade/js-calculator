e4fdbf673368760c3c774e10253ad66e85e9c0bd
Hélen Andrade <helenaraujoandrade@hotmail.com>
2025-09-30 10:35:34 -0300

    feat: add tailwind

--- main/main.js
+++ main/main.js
@@ -1,1 +1,16 @@
-console.log("Hello from Electron")
+// In JS convention, PascalCase modules are instantiable class constructors
+// whereas camelCase modules are not instantiable
+const { app, BrowserWindow, ipcMain, Menu } = require("electron");
+const path = require("node:path");
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 350,
+    height: 600
+  });
+  win.loadFile("renderer/index.html");
+  Menu.setApplicationMenu(null);
+};
+
+app.whenReady().then(() => {
+  createWindow();
+});
